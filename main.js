b6277dcc0077140c9175dba14a471bca7c8b5f4b
tyrone17 <tyronemaasdorp@gmail.com>
2022-07-16 19:38:12 +0200

    Code to replace null with 0 as value in array

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ if (form !=""){
     console.log("yes");
     array = document.getElementsByClassName("form-control");
     //because of this the id must therefore = label
-    array = array[2].ariaLabel + ", " +  array[2].ariaValueText
+    array = array[2].ariaLabel + ", " +  array[2].ariaValueText;
     //the above creates a value pair of the indexed consisiting of the label(which is the id value)
     // and the value which if empty, returns null
     JSON.stringify(array);
-    console.log(array)
-  
-   
+    array.substring(1, array.length-1);
+    console.log(array);
+    let result =  array.replace(", null", ", 0");
+    console.log(result);
+    
 } else {
     console.log("fail");
 }
