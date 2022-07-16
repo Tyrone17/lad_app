b91673f1470badca6808684eec0aab966bf05a86
tyrone17 <tyronemaasdorp@gmail.com>
2022-07-16 16:54:47 +0200

    Updated the js & HTML to use id as label and value pairs

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 'use strict';
 
 let form = document.getElementsByName("form");
+let array
 
 if (form !=""){
     console.log("yes");
-    let array = [];
     array = document.getElementsByClassName("form-control");
-    //console.log(array);
-    console.log(array[1]);
+    //because of this the id must therefore = label
+    array = array[2].ariaLabel + ", " +  array[2].ariaValueText
+    //the above creates a value pair of the indexed consisiting of the label(which is the id value)
+    // and the value which if empty, returns null
+    JSON.stringify(array);
+    console.log(array)
+  
    
 } else {
     console.log("fail");
