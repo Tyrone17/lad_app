ec4d1ff233926281f6bc78a8b2f50a89c7eb1b67
tyrone17 <tyronemaasdorp@gmail.com>
2022-07-17 14:27:17 +0200

    Updated the js script to create an array of inputs from form

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 'use strict';
 
 let form = document.getElementsByName("form");
-let array
+let array,Formdata, i
 
 if (form !=""){
-    console.log("yes");
+    // console.log("yes");
+    // array = document.getElementsByClassName("form-control");
+    // //because of this the id must therefore = label
+    // array = array[2].ariaLabel + ", " +  array[2].ariaValueText;
+    // //the above creates a value pair of the indexed consisiting of the label(which is the id value)
+    // // and the value which if empty, returns null
+    // JSON.stringify(array);
+    // array.substring(1, array.length-1);
+    // console.log(array);
+    // let result =  array.replace(", null", ", 0");
+    // console.log(result);
     array = document.getElementsByClassName("form-control");
-    //because of this the id must therefore = label
-    array = array[2].ariaLabel + ", " +  array[2].ariaValueText;
-    //the above creates a value pair of the indexed consisiting of the label(which is the id value)
-    // and the value which if empty, returns null
-    JSON.stringify(array);
-    array.substring(1, array.length-1);
-    console.log(array);
-    let result =  array.replace(", null", ", 0");
-    console.log(result);
+    
+    Formdata = []
+    for (let i = 0; i < 14; i++) {
+        Formdata.push(array[i].ariaLabel + ", " +  array[i].ariaValueText);
+    };
+    //console.log(array);
+    console.log(Formdata)
     
 } else {
     console.log("fail");
